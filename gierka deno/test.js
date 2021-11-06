c59e900ea537c38c4144335ad2337ac2cef3105a
Iidontknown <jakub-strugala@wp.pl>
2021-11-06 22:36:41 +0100

    Delete gierka deno directory

--- gierka deno/test.js
+++
@@ -1,4 +0,0 @@
-import { listenAndServe } from "https://deno.land/std@0.113.0/http/server.ts";
-
-console.log("http://localhost:8000/");
-listenAndServe(":8000", (req) => new Response("Hello World\n"));
