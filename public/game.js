9621e50ec2f0c9426c84b1af42e375688dd552b4
zulux99 <szymon14059@gmail.com>
2021-11-28 16:40:26 +0100

    nie działające tilesety

--- public/game.js
+++ public/game.js
@@ -25,22 +25,19 @@ const config = {
 game = new Phaser.Game(config);
 
 function preload() {
-    this.load.image('tiles', 'public/tile/drawtiles-spaced.png');
-    // this.load.spritesheet('postac', 'public/tile/postackopia.png', {
-    //     frameWidth: 32,
-    //     frameHeight: 32,
-    //   });
-    this.load.image('postac', 'public/assets/super_mario.png')
-      this.load.tilemapTiledJSON("map", "assets/super_mario.json");
-    // this.load.tilemapCSV('map', 'https://raw.githubusercontent.com/photonstorm/phaser3-examples/master/public/assets/tilemaps/csv/grid.csv');
+    this.load.image('tiles', 'tile/drawtiles-spaced.png');
+    this.load.spritesheet('postac', 'tile/postackopia.png', {
+        frameWidth: 32,
+        frameHeight: 32,
+      });
+    this.load.tilemapCSV('map', 'https://raw.githubusercontent.com/photonstorm/phaser3-examples/master/public/assets/tilemaps/csv/grid.csv');
 }
 
 function create() {
     const map = this.make.tilemap({ key: 'map', tileWidth: 32, tileHeight: 32 });
     tileset = map.addTilesetImage('tiles', null, 32, 32, 1, 2);
     layer = map.createStaticLayer(0, tileset, 0, 0);
-    // player = this.add.sprite(32 + 16, 32 + 16, 'postac');
-    // layer.setCollisionBetween(1, 50);
+    layer.setCollisionBetween(1, 50);
     player = this.physics.add.sprite(48, 48, "postac");
     player.setScale(.95);
     this.physics.add.collider(player, layer);
