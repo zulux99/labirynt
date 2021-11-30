a21bb75ff8d296ddb62166a4b6ad731c5d2e2fbb
zulux99 <83884087+zulux99@users.noreply.github.com>
2021-11-30 12:30:46 +0100

    movement na fizyce phasera

--- public/game.js
+++ public/game.js
@@ -2,40 +2,36 @@ const config = {
     type: Phaser.AUTO,
     width: 1000,
     height: 800,
-    parent: 'phaser-example',
+    parent: "phaser-example",
     pixelArt: true,
-    backgroundColor: '#1a1a2d',
-    plugins: {
-        scene: [{
-            key: "gridEngine",
-            plugin: GridEngine,
-            mapping: "gridEngine",
-          }],
-      },
+    backgroundColor: "#1a1a2d",
     physics: {
-        default: 'arcade',
+        default: "arcade",
     },
     scene: {
         preload: preload,
         create: create,
-        update: update
-    }
+        update: update,
+    },
 };
 
 game = new Phaser.Game(config);
 
 function preload() {
-    this.load.image('tiles', 'tile/drawtiles-spaced.png');
-    this.load.spritesheet('postac', 'tile/postackopia.png', {
+    this.load.image("tiles", "tile/drawtiles-spaced.png");
+    this.load.spritesheet("postac", "tile/postackopia.png", {
         frameWidth: 32,
         frameHeight: 32,
-      });
-    this.load.tilemapCSV('map', 'https://raw.githubusercontent.com/photonstorm/phaser3-examples/master/public/assets/tilemaps/csv/grid.csv');
+    });
+    this.load.tilemapCSV(
+        "map",
+        "https://raw.githubusercontent.com/photonstorm/phaser3-examples/master/public/assets/tilemaps/csv/grid.csv",
+    );
 }
 
 function create() {
-    const map = this.make.tilemap({ key: 'map', tileWidth: 32, tileHeight: 32 });
-    tileset = map.addTilesetImage('tiles', null, 32, 32, 1, 2);
+    const map = this.make.tilemap({ key: "map", tileWidth: 32, tileHeight: 32 });
+    tileset = map.addTilesetImage("tiles", null, 32, 32, 1, 2);
     layer = map.createStaticLayer(0, tileset, 0, 0);
     layer.setCollisionBetween(1, 50);
     player = this.physics.add.sprite(48, 48, "postac");
@@ -43,28 +39,19 @@ function create() {
     this.physics.add.collider(player, layer);
     this.cameras.main.startFollow(player, true);
     this.cameras.main.setFollowOffset(-player.width, -player.height);
-    const gridEngineConfig = {
-        characters: [
-          {
-            id: "postac",
-            sprite: player,
-            walkingAnimationMapping: 6,
-            walkingAnimationEnabled: true,
-            startPosition: { x: 8, y: 8 },
-          },
-        ],
-      };
-    this.gridEngine.create(map, gridEngineConfig);
+    cursors = this.input.keyboard.createCursorKeys();
 }
-function update() {
-    const cursors = this.input.keyboard.createCursorKeys();
-    if (cursors.left.isDown) {
-      this.gridEngine.move("player", "left");
-    } else if (cursors.right.isDown) {
-      this.gridEngine.move("player", "right");
-    } else if (cursors.up.isDown) {
-      this.gridEngine.move("player", "up");
-    } else if (cursors.down.isDown) {
-      this.gridEngine.move("player", "down");
-    }
-  }
+function update(){
+    if (cursors.left.isDown) 
+        player.setVelocityX(-100);
+    else if (cursors.right.isDown)
+        player.setVelocityX(100);
+    else 
+        player.setVelocityX(0);
+    if (cursors.down.isDown)
+        player.setVelocityY(100);
+    else if (cursors.up.isDown)
+        player.setVelocityY(-100);
+    else
+        player.setVelocityY(0);
+}
