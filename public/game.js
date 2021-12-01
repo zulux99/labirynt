12c815608a9ee0f138b2edef90e689f1f1b9e7ad
zulux99 <83884087+zulux99@users.noreply.github.com>
2021-12-01 13:01:24 +0100

    komentarze i fullscreen

--- public/game.js
+++ public/game.js
@@ -1,7 +1,7 @@
 const config = {
     type: Phaser.AUTO,
-    width: 1000,
-    height: 800,
+	width: window.innerWidth,
+	height: window.innerHeight,
     parent: "phaser-example",
     pixelArt: true,
     backgroundColor: "#1a1a2d",
@@ -18,11 +18,15 @@ const config = {
 game = new Phaser.Game(config);
 
 function preload() {
+
+    // ściany
     this.load.image("tiles", "tile/drawtiles-spaced.png");
+    // postac
     this.load.spritesheet("postac", "tile/postackopia.png", {
         frameWidth: 32,
         frameHeight: 32,
     });
+    // mapa
     this.load.tilemapCSV(
         "map",
         "https://raw.githubusercontent.com/photonstorm/phaser3-examples/master/public/assets/tilemaps/csv/grid.csv",
@@ -30,15 +34,25 @@ function preload() {
 }
 
 function create() {
+
+    // kafelki, mapa, warstwy, kolizja
     const map = this.make.tilemap({ key: "map", tileWidth: 32, tileHeight: 32 });
     tileset = map.addTilesetImage("tiles", null, 32, 32, 1, 2);
     layer = map.createStaticLayer(0, tileset, 0, 0);
     layer.setCollisionBetween(1, 50);
+
+    // animacje
     cursors = this.input.keyboard.createCursorKeys(); this.anims.create({
         key: 'idle',
         frames: this.anims.generateFrameNumbers('postac', { frames: [1] }),
         frameRate: 0,
         repeat: 1,
+    });
+    this.anims.create({
+        key: 'walk_left',
+        frames: this.anims.generateFrameNumbers('postac', { frames: [3, 4, 5] }),
+        frameRate: 6,
+        repeat: -1
     });
     this.anims.create({
         key: 'walk_right',
@@ -58,19 +72,20 @@ function create() {
         frameRate: 6,
         repeat: -1,
     });
-    this.anims.create({
-        key: 'walk_left',
-        frames: this.anims.generateFrameNumbers('postac', { frames: [3, 4, 5] }),
-        frameRate: 6,
-        repeat: -1
-    });
+
+    // tworzenie postaci, ruchy kamery
     player = this.physics.add.sprite(48, 48, "postac");
-    player.setScale(.5);
+    player.setScale(.8);
     this.physics.add.collider(player, layer);
     this.cameras.main.startFollow(player, true);
     this.cameras.main.setFollowOffset(-player.width, -player.height);
+    this.cameras.main.setDeadzone(64, 64);
+    this.cameras.main.setZoom(1.5);
 }
+
 function update() {
+
+    // poruszanie się po mapie
     if (cursors.left.isDown) {
         player.play("walk_left", true);
         player.setVelocity(-100, 0);
@@ -91,4 +106,6 @@ function update() {
         player.setVelocity(0, 0);
         player.play("idle");
     }
+
+
 }
